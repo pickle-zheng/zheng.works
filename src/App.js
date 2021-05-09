83bca4e804a420d7ae64a06d5b364f2653b5dd6b
Zheng Zhao <zheng.zhao@playgroundxyz.com>
2021-05-09 14:59:05 +1000

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import HeroSection from "./container/HeroSection/HeroSection";
+
+const App = () => {
+  return (
+    <div className='App'>
+      <HeroSection />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/container/HeroSection/HeroSection.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <div>
+      <h1>ZHENG</h1>
+    </div>
+  );
+};
+
+export default HeroSection;
