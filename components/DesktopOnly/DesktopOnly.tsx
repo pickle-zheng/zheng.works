00b26dbe9a3577175c795a063f12fb04bf9e13f0
Zheng Zhao <fixfish@gmail.com>
2022-04-27 08:32:17 +1000

    copy update

--- components/DesktopOnly/DesktopOnly.tsx
+++ components/DesktopOnly/DesktopOnly.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "./DesktopOnly.module.css";
+
 const DesktopOnly = () => {
   return (
     <div className={styles.desktopOnly}>
-      <img src='/images/logo.png' />
-      <p> I've only built it so far to</p>
+      <img src='/images/logo.png' alt='logo' />
+      <p> I have only built it so far to</p>
       <h1>
         Support
         <br />
