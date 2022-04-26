71606d3751f57f47ed65200e6b475c86183e65cb
pickle_zheng <fixfish@gmail.com>
2022-04-26 22:24:54 +1000

    ga update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,35 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useEffect } from "react";
 import Canvas from "../components/Canvas/Canvas";
 import Instruction from "../components/Instruction/Instruction";
+import { pageview } from "../utils/ga";
 import styles from "./portfolio.module.css";
-import ReactGA from "react-ga";
-import { useEffect } from "react";
 
 const Home: NextPage = () => {
   useEffect(() => {
-    ReactGA.initialize("G-D3MKH4NHZ0");
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  });
+    pageview("/portfolio-index");
+  }, []);
   return (
     <div className={styles.container}>
       <Head>
         <title>zz</title>
         <meta property='og:title' content='Game - Zheng Works' key='title' />
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-D3MKH4NHZ0`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-D3MKH4NHZ0');
+            `
+          }}
+        />
       </Head>
       <Canvas mode='portfolio' />
       <Instruction />
