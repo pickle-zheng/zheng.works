e34d1ee1e31f61a7ce6c281f45099ac9668ed83e
Zheng Zhao <fixfish@gmail.com>
2022-04-28 09:53:18 +1000

    game setup

---
+++ components/SignUp/SignUp.tsx
@@ -0,0 +1,148 @@
+import React, { Dispatch, SetStateAction, useState } from "react";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import { userInfo } from "../../pages/game";
+import styles from "./SignUp.module.css";
+
+const SignUp = ({
+  setUserInfo,
+  setSessionId
+}: {
+  setUserInfo: Dispatch<SetStateAction<userInfo | null>>;
+  setSessionId: Dispatch<SetStateAction<string | null>>;
+}) => {
+  const [step, setStep] = useState(1);
+  return (
+    <div className={styles.signUp}>
+      <div className={styles.wrapper}>
+        <h1>G'day, Socceroo 🦘</h1>
+        {step === 0 ? (
+          <UserInfoForm setUserInfo={setUserInfo} setStep={setStep} />
+        ) : (
+          <SessionForm setSessionId={setSessionId} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+const UserInfoForm = ({
+  setUserInfo,
+  setStep
+}: {
+  setUserInfo: Dispatch<SetStateAction<userInfo | null>>;
+  setStep: Dispatch<SetStateAction<number>>;
+}) => {
+  return (
+    <Formik
+      initialValues={{ name: "", carType: "", score: 0 }}
+      validate={(values) => {
+        const errors: { name?: string } = {};
+        if (!values.name) {
+          errors.name = "Required";
+        } else if (values.name.length > 2) {
+          errors.name = "Must be 2 letters or less";
+        } else if (!values.carType) {
+          errors.name = "Required ";
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setUserInfo(values);
+        setStep(1);
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({ isSubmitting, values }) => (
+        <Form>
+          <p>Your initials (2 letters)</p>
+          <Field type='text' name='name' />
+          <ErrorMessage name='email' component='div' />
+          <br />
+          <p>Choose your car</p>
+          <div
+            className={styles.carType}
+            role='group'
+            aria-labelledby='my-radio-group'
+          >
+            <label
+              className={
+                values.carType === "pickup" ? styles.selected : undefined
+              }
+            >
+              <Field type='radio' name='carType' value='pickup' />
+              <img src='/images/pickup.png' alt='pickup' />
+              Raptor
+            </label>
+            <label
+              className={
+                values.carType === "sedan" ? styles.selected : undefined
+              }
+            >
+              <Field type='radio' name='carType' value='sedan' />
+              <img src='/images/sedan.png' alt='sedan' />
+              Sedane
+            </label>
+            <label
+              className={
+                values.carType === "jeep" ? styles.selected : undefined
+              }
+            >
+              <Field type='radio' name='carType' value='jeep' />
+              <img src='/images/jeep.png' alt='jeep' />
+              Rooney
+            </label>
+          </div>
+          <button type='submit' disabled={isSubmitting}>
+            Next
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+const SessionForm = ({
+  setSessionId
+}: {
+  setSessionId: Dispatch<SetStateAction<string | null>>;
+}) => {
+  return (
+    <Formik
+      initialValues={{ id: "" }}
+      validate={(values) => {
+        const errors: { name?: string } = {};
+        if (!values.id) {
+          errors.name = "Required";
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setSessionId(values.id);
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({ isSubmitting, values }) => (
+        <Form>
+          <p>Enter your Session ID</p>
+          <Field type='text' name='id' placeholder='xxxx-xxxx-xxx' />
+          <ErrorMessage name='id' component='div' />
+          <br />
+          <button type='submit' disabled={isSubmitting || values.id === ""}>
+            Start 🔥
+          </button>
+          <button type='submit' disabled={isSubmitting} className={styles.host}>
+            Host one
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default SignUp;
--- pages/game.tsx
+++ pages/game.tsx
@@ -1,17 +1,35 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useEffect, useState } from "react";
 import Canvas from "../components/Canvas/Canvas";
 import Instruction from "../components/Instruction/Instruction";
+import SignUp from "../components/SignUp/SignUp";
 import styles from "./game.module.css";
 
+export type userInfo = {
+  name: string;
+  score: number;
+  carType: string;
+};
+
 const Game: NextPage = () => {
+  const [userInfo, setUserInfo] = useState<userInfo | null>(null);
+  const [sessionId, setSessionId] = useState<string | null>(null);
+  useEffect(() => {
+    console.log(userInfo);
+  }, [userInfo]);
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Game - Zheng Works</title>
+        <title>zz - 🔥</title>
         <meta property='og:title' content='Game - Zheng Works' key='title' />
       </Head>
-      <Canvas mode='game' />
+      {userInfo && sessionId ? (
+        <Canvas mode='game' />
+      ) : (
+        <SignUp setUserInfo={setUserInfo} setSessionId={setSessionId} />
+      )}
       <Instruction />
     </div>
   );
